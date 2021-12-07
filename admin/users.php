fa133f4972a71873ddd1101d20135ad0f603d59c
Dormapsrb <Dormapasaribu7@gmail.com>
2021-12-08 03:40:37 +0700

    user1

--- admin/users.php
+++ admin/users.php
@@ -32,8 +32,6 @@ include("sidebar.php");
           <h2><b>Data User</b></h2>
         </div>
         <div class="col-sm-6">
-          <!-- <a href="#adddataModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Tambah User</span></a>
-          <a href="#deletedataModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Hapus</span></a> -->
         </div>
       </div>
     </div>
@@ -63,12 +61,6 @@ include("sidebar.php");
           <td><?php echo $pecah['phone_number']; ?></td>
           <td><?php echo $pecah['paket']; ?></td>
           <td>
-            <!-- <a data-target="#editdataModal" id="editdataModal class="edit" id="editdata" data-toggle="modal" data-id="<?= $pecah['user_id'];?>"
-            data-user="<?= $pecah['username'];?>" data-emaill="<?= $pecah['email'];?>" data-date="<?= $pecah['date_of_birth'];?>"
-            data-genderr="<?= $pecah['gender'];?>" data-phone="<?= $pecah['phone_number '];?>" data-pakett="<?= $pecah['paket'];?>"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-             -->
-
-            <a href="function/hapususer.php?id=<?php echo $pecah['id_user'];?>" onclick="return confirm('Apakah yakin ingin menghapus?'"  class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
           </td>
         </tr>
       </tbody>
@@ -87,110 +79,6 @@ include("sidebar.php");
         <li class="page-item"><a href="#" class="page-link">5</a></li>
         <li class="page-item"><a href="#" class="page-link">Next</a></li>
       </ul>
-    </div>
-  </div>
-</div>
-<!-- Edit Modal HTML -->
-<div id="adddataModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form>
-        <div class="modal-header">
-          <h4 class="modal-title">Add User</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        </div>
-        <div class="modal-body">
-          <div class="form-group">
-            <label>User ID</label>
-            <input type="text" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label>Username</label>
-            <input type="text" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label>Email</label>
-            <input type="email" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label>Paket</label>
-            <input type="text" class="form-control" required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-          <input type="submit" class="btn btn-success" value="Add">
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-<!-- Edit Modal HTML -->
-<div id="editdataModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form action="edituser.php" method="post">
-      <input type="hidden" name="user_id" id="user_id">
-        <div class="modal-header">
-          <h4 class="modal-title">Edit User</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        </div>
-        <div class="modal-body">
-          <div class="form-group">
-            <label for="user_id">User ID</label>
-            <input type="number" class="form-control" name="user_id" id="user_id" required>
-          </div>
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" name="username" id="username" required>
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" name="email" id="email" required>
-          </div>
-          <div class="form-group">
-            <label for="date_of_birth">Tanggal Lahir</label>
-            <input type="date" class="form-control" name="date_of_birth" id="date_of_birth" required>
-          </div>
-          <div class="form-group">
-            <label for="gender">Jenis Kelamin</label>
-            <input type="gender" class="form-control" name="gender" id="gender" required>
-          </div>
-          <div class="form-group">
-            <label for="phone_number">No Telp</label>
-            <input type="number" class="form-control" name="phone_number" id="phone_number" required>
-          </div>
-          <div class="form-group">
-            <label for="paket">Paket</label>
-            <input type="text" class="form-control" name="paket" id="paket"required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-          <input type="submit" class="btn btn-info" value="Save">
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-<!-- Delete Modal HTML -->
-<div id="deletedataModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form>
-        <div class="modal-header">
-          <h4 class="modal-title">Hapus User</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        </div>
-        <div class="modal-body">
-          <p>Apakah kamu yakin untuk menghapus data ini?</p>
-          <p class="text-warning"><small>Aksi ini akan menghilangkan data dan tidak bisa dikembalikan</small></p>
-        </div>
-        <div class="modal-footer">
-          <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-          <input type="submit" class="btn btn-danger" value="Delete">
-        </div>
-      </form>
     </div>
   </div>
 </div>
